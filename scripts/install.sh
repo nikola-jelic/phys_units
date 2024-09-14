b945ba2804c5c4a9c4938939d49e2057f679338a
nemanjadjekic <ndjekic@proton.me>
2024-09-15 00:14:32 +0200

    Add initial CMake system

---
+++ scripts/install.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+echo "Install library"
+
+# Check if build folder exists
+if [ -d "../build" ]
+then
+    echo "Removing old build folder..."
+    rm -rf
+else
+    echo "Creating build folder..."
+    mkdir build
+fi
+
+cd build
+
+cmake .. -DCMAKE_INSTALL_PREFIX=/usr
+
+sudo cmake --install .
